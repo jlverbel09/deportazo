85238d8c6a59adfd0008aa8150b068b01404b17d
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-17 02:50:27 +0100

    ajustes

--- dist/pages/services/voleybol_actions.php
+++ dist/pages/services/voleybol_actions.php
@@ -120,12 +120,12 @@ if (isset($_POST['accion']) && $_POST['accion'] == 'guardarEnfrentamiento') {
     $stm->execute([
         $_POST['cant_equipos'],
         'A',
-        'A-'. $_POST['equipo_1'],
-        'B-'. $_POST['equipo_2'],
-        'C-'. $_POST['equipo_3'],
-        'D-'. $_POST['equipo_4'],
-        'E-'. $_POST['equipo_5'],
-        'F-'. $_POST['equipo_6'],
+        'A-' . $_POST['equipo_1'],
+        'B-' . $_POST['equipo_2'],
+        'C-' . $_POST['equipo_3'],
+        'D-' . $_POST['equipo_4'],
+        'E-' . $_POST['equipo_5'],
+        'F-' . $_POST['equipo_6'],
         date('Y-m-d')
     ]);
 
@@ -180,7 +180,7 @@ if (isset($_GET['accion']) && $_GET['accion'] == 'cargarEnfrentamiento') {
             (select equipo5 from enfrentamiento_rapido where estado = 'A')
             when `visitante` = 'F' then 
             (select equipo6 from enfrentamiento_rapido where estado = 'A')
-            end as visitante,estado
+            end as visitante,estado,ganador
         from guia_enfrentamiento_rapido ger where cant_equipos = (select cantidad_equipos   from enfrentamiento_rapido er where estado = 'A' )")->fetchAll();
 
     $j = 0;
@@ -206,21 +206,43 @@ if (isset($_GET['accion']) && $_GET['accion'] == 'cargarEnfrentamiento') {
                    <b>&nbsp;&nbsp;Descansa<b>
                 </td>';
         } else {
-            $btnEstado = "btn-outline-light text-light";
-            $estado = 'bg-success';
-            $icono = 'check';
+
             if ($l['estado'] == 'A') {
-                $estado = '';
-                $btnEstado = "btn-outline-secondary text-secondary";
-                $icono = 'square';
+            } else {
+
+
+                
             }
-            $td = '<td class="d-flex align-items-center ' . $estado . '">
-             <button class="btn ' . $btnEstado . ' me-2" onclick="cambiarEstado(' . $l['id'] . ')"><i class="fa fa-' . $icono . '"></i></button>
-                    <input type="text" readonly value="' . strtoupper($l['local']) . '" class="form-control" >
-                    <b>&nbsp;&nbsp;VS&nbsp;&nbsp;</b>    
-                    <input type="text" readonly value="' . strtoupper($visitante) . '" class="form-control" >
-                     <button class="btn ' . $btnEstado . ' ms-2" onclick="cambiarEstado(' . $l['id'] . ')"><i class="fa fa-' . $icono . '"></i></button>
-                </td>';
+
+            if (explode('-', $l['local'])[0] == $l['ganador']) {
+                $estadoLocal = 'bg-success';
+                $btnEstadoLocal = "btn-outline-secondary text-light";
+                $iconolocal = 'check';
+            } else {
+                $estadoLocal = '';
+                $btnEstadoLocal = "btn-outline-black text-dark";
+                $iconolocal = 'square';
+            }
+            if (explode('-', $l['visitante'])[0] == $l['ganador']) {
+                $estadoVisitante = 'bg-success';
+                $btnEstadovisi = "btn-outline-secondary text-light";
+                $iconovis = 'check';
+            } else {
+                $estadoVisitante = '';
+                $btnEstadovisi = "btn-outline-dark text-dark";
+                $iconovis = 'square';
+            }
+            $td = '<td class="d-flex align-items-center ' . $estadoLocal . '">
+                        <button class=" border-white btn ' . $btnEstadoLocal . ' me-2" onclick="cambiarEstado(' . $l['id'] . ',\'' . $l['local'] . '\')"><i class="  fa fa-' . $iconolocal . '"></i></button>
+                        <input type="text" readonly value="' . strtoupper($l['local']) . '" class="form-control" >
+                    </td>
+
+                    <td class=" text-center"><b>&nbsp;&nbsp;VS&nbsp;&nbsp;</b>  </td>  
+
+                    <td class="d-flex align-items-center ' . $estadoVisitante . '">
+                        <input type="text" readonly value="' . strtoupper($visitante) . '" class="form-control" >
+                        <button class=" border-white btn ' . $btnEstadovisi . ' ms-2" onclick="cambiarEstado(' . $l['id'] . ',\'' . $l['visitante'] . '\')"><i class="fa fa-' . $iconovis . '"></i></button>
+                    </td>';
         }
 
         $html .= '
@@ -246,21 +268,105 @@ if (isset($_GET['accion']) && $_GET['accion'] == 'cargarEnfrentamiento') {
 }
 
 if (isset($_GET['accion']) && $_GET['accion'] == 'cambiarEstado') {
-    $restms = $conexion->query("update guia_enfrentamiento_rapido  set estado = 'F' where id = ".$_GET['id']);
+
+    $equipo = $_GET['equipo'];
+    $equipo = explode('-', $equipo);
+    $equipo = $equipo[0];
+    if ($equipo == 'A') {
+        $campoPunt = "update  enfrentamiento_rapido set  punt_equipo1 = (select punt_equipo1 +3 from enfrentamiento_rapido er where estado = 'A') where estado = 'A'";
+    } else if ($equipo == 'B') {
+        $campoPunt = "update  enfrentamiento_rapido set  punt_equipo2 = (select punt_equipo2 +3 from enfrentamiento_rapido er where estado = 'A') where estado = 'A'";
+    } else if ($equipo == 'C') {
+        $campoPunt = "update  enfrentamiento_rapido set  punt_equipo3 = (select punt_equipo3 +3 from enfrentamiento_rapido er where estado = 'A') where estado = 'A'";
+    } else if ($equipo == 'D') {
+        $campoPunt = "update  enfrentamiento_rapido set  punt_equipo4 = (select punt_equipo4 +3 from enfrentamiento_rapido er where estado = 'A') where estado = 'A'";
+    } else if ($equipo == 'E') {
+        $campoPunt = "update  enfrentamiento_rapido set  punt_equipo5 = (select punt_equipo5 +3 from enfrentamiento_rapido er where estado = 'A') where estado = 'A'";
+    } else if ($equipo == 'F') {
+        $campoPunt = "update  enfrentamiento_rapido set  punt_equipo6 = (select punt_equipo6 +3 from enfrentamiento_rapido er where estado = 'A') where estado = 'A'";
+    }
+
+    $restms = $conexion->query("update guia_enfrentamiento_rapido  set estado = 'F', ganador = '$equipo' where id = " . $_GET['id']);
     if ($restms) {
+        $puntaje = $conexion->query($campoPunt);
         $data->estado = 'success';
         $data->mensaje = 'Enfrentamiento finalizado correctamente';
     }
 }
 
 if (isset($_GET['accion']) && $_GET['accion'] == 'restaurarReencuentros') {
-    $restms = $conexion->query("update guia_enfrentamiento_rapido  set estado = 'A' ");
+    $restms = $conexion->query("update guia_enfrentamiento_rapido  set estado = 'A' , ganador = null where cant_equipos = (select cantidad_equipos   from enfrentamiento_rapido er where estado = 'A' )");
     if ($restms) {
+        $conexion->query("update  enfrentamiento_rapido set  punt_equipo1 = 0, punt_equipo2=0, punt_equipo3=0, punt_equipo4=0, punt_equipo5=0, punt_equipo6=0 where estado = 'A'");
         $data->estado = 'success';
         $data->mensaje = 'Enfrentamientos reestablecidos correctamente';
     }
 }
 
+if (isset($_GET['accion']) && $_GET['accion'] == 'cargarTablaEnfrentamientosRapidos') {
 
+    $cantidad_equipos = $_GET['cantidad_equipos'];
+    $resEnfRapidos = $conexion->query("select * from (
+select 1 as posicion, equipo1 as equipo , punt_equipo1 as puntos  from enfrentamiento_rapido where estado  = 'A'
+union
+select 2 as posicion, equipo2 as equipo , punt_equipo2  as puntos from enfrentamiento_rapido where estado  = 'A'
+union 
+select 3 as posicion,equipo3 as equipo , punt_equipo3  as puntos from enfrentamiento_rapido where estado  = 'A'
+union
+select 4 as posicion,equipo4 as equipo , punt_equipo4  as puntos from enfrentamiento_rapido where estado  = 'A'
+union 
+select 5 as posicion,equipo5 as equipo , punt_equipo5  as puntos from enfrentamiento_rapido where estado  = 'A'
+union 
+select 6 as posicion,equipo6 as equipo , punt_equipo6  as puntos from enfrentamiento_rapido where estado  = 'A'
+) j order by 3 desc,1 limit " . $cantidad_equipos)->fetchAll();
+
+    $tablaRapida = [];
+
+
+
+    $tablaHtml = '<table class="table table-bordered mt-3">
+                    <thead>
+                        <tr>
+                            <th colspan="3">JUEGO RÁPIDO</th>
+                        </tr>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Equipo</th>
+                            <th scope="col"class="text-center">Pts</th>
+                        </tr>
+                    </thead>
+                    <tbody>';
+
+
+    foreach ($resEnfRapidos as $r) {
+        static $i = 0;
+        if ($i == 0) {
+            $estado = 'bg-success text-white';
+        } else {
+            $estado = '';
+        }
+        $i++;
+
+
+        $tablaHtml .= '
+              <tr >
+                            <th class="' . $estado . '" scope="row" >' . $i . '</th>
+                            <td class="nombreEquipoTabla  ' . $estado . '">
+                                ' . strtoupper($r['equipo']) . '
+                            </td>
+                            <td class="text-center ' . $estado . '">' . $r['puntos'] . '</td>
+                        </tr>';
+    }
+
+    $tablaHtml .= '
+                     
+                    </tbody>
+                </table>';
+
+    $data->estado = 'success';
+    $data->mensaje = 'Tabla de enfrentamientos rapidos';
+    $data->tabla = $tablaHtml;
+    $data->body = $tablaRapida;
+}
 
 echo json_encode($data);
