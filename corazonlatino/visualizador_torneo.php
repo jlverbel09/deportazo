dce3ab47a46face97e878bb3fc740b0f1107da9f
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-05-03 00:16:57 +0200

    ajustes

---
+++ corazonlatino/visualizador_torneo.php
@@ -0,0 +1,3 @@
+<body style="margin: 0;padding: 0;overflow: hidden;">
+    <iframe style="width: 100%; height: 100%;margin:0" src="../dist/pages/services/visualizador_general.php?torneo=13" frameborder="0"></iframe>
+</body>
