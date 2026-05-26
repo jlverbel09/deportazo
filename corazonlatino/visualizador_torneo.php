e6d336cb1b8fce150505793eca51caaf0e3959c5
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-05-26 21:43:38 +0200

    nuevo torneo

--- corazonlatino/visualizador_torneo.php
+++ corazonlatino/visualizador_torneo.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body style="margin: 0;padding: 0;overflow: hidden;">
-    <iframe style="width: 100%; height: 100%;margin:0" src="../dist/pages/services/visualizador_general.php?torneo=13" frameborder="0"></iframe>
+    <iframe style="width: 100%; height: 100%;margin:0" src="../dist/pages/services/visualizador_general.php?torneo=14" frameborder="0"></iframe>
 </body>
 
 </html>
